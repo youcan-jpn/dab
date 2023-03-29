78e9d0983f7fba633b0d8aee2ee79d9e1134fa6b
yuta ono <youcana40627@gmail.com>
2023-03-29 23:19:28 +0900

    b/api: add get categories

---
+++ backend/src/app/api/get_categories.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/youcan-jpn/dab/backend/gen/oapi"
+	"github.com/youcan-jpn/dab/backend/src/app/service"
+)
+
+func (a *API) GetCategories(ctx echo.Context) error {
+	txn, err := a.dbClient.BeginTx(a.ctx, &sql.TxOptions{
+		Isolation: sql.LevelDefault,
+		ReadOnly:  true,
+	})
+	if err != nil {
+		return err
+	}
+
+	cas, err := service.GetCategories(a.ctx, txn)
+	if err != nil {
+		return err
+	}
+
+	cs := make([]oapi.Category, 0, len(cas))
+	for _, c := range cas {
+		ca := oapi.Category{
+			CategoryId: &c.CategoryID,
+			CategoryName: &c.CategoryName,
+		}
+		cs = append(cs, ca)
+	}
+
+	return ctx.JSON(http.StatusOK, oapi.Categories{Categories: &cs})
+}
