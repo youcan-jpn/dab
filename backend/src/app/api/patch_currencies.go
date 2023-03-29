52081791a97622489494077c6ecfe6c03aaf7bd8
yuta ono <youcana40627@gmail.com>
2023-03-29 23:47:20 +0900

    b/api: add patch currencies

---
+++ backend/src/app/api/patch_currencies.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/youcan-jpn/dab/backend/gen/oapi"
+	"github.com/youcan-jpn/dab/backend/src/app/service"
+)
+
+func (a *API) PatchCurrenciesCurrencyId(ctx echo.Context, currencyId oapi.CurrencyId) error {
+	var cu oapi.Currency
+	err := ctx.Bind(&cu)
+	if err != nil {
+		return ctx.String(http.StatusBadRequest, err.Error())
+	}
+
+	txn, err := a.dbClient.BeginTx(a.ctx, &sql.TxOptions{
+		Isolation: sql.LevelDefault,
+		ReadOnly:  false,
+	})
+	defer txn.Rollback()
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	err = service.PatchCurrency(a.ctx, txn, currencyId, *cu.CurrencyName, *cu.InYen)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	res, err := service.GetOneCurrencyByID(a.ctx, txn, currencyId)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	if err = txn.Commit(); err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	ma := timeToString(res.ModifiedAt)
+	ca := timeToString(res.CreatedAt)
+	return ctx.JSON(http.StatusOK, oapi.Currency{
+		CurrencyId: &res.CurrencyID,
+		CurrencyName: &res.CurrencyName,
+		InYen: &res.InYen,
+		ModifiedAt: &ma,
+		CreatedAt: &ca,
+	})
+}
