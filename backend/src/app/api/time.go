8c0ef89d9534e9b408415bff6b609ec71bad9b7b
yuta ono <youcana40627@gmail.com>
2023-03-30 14:33:03 +0900

    b/api: add function for transformation of time

--- backend/src/app/api/time.go
+++ backend/src/app/api/time.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"reflect"
 	"time"
+
+	"github.com/youcan-jpn/dab/backend/gen/oapi"
 )
 
 var layout = "2006-01-02 15:04:05"
@@ -9,4 +12,24 @@ var layout = "2006-01-02 15:04:05"
 func timeToString(t *time.Time) string {
 	str := t.Format(layout)
 	return str
+}
+
+func objToTime(o *oapi.Date, tz *time.Location) *time.Time {
+	if (o == nil) || reflect.ValueOf(o).IsNil() {
+		return nil
+	}
+	y := o.Year
+	if *y < 1 || *y > 9999 {
+		return nil
+	}
+	m := time.Month(*o.Month)
+	if m < 1 || m > 12 {
+		return nil
+	}
+	d := o.Day
+	if *d < 1 || *d > 31 {
+		return nil
+	}
+	t := time.Date(*y, m, *d, 0, 0, 0, 0, tz)
+	return &t
 }
