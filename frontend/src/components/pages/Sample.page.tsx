4a62896586e99389a2c155050fc5a55520632a10
yuta ono <youcana40627@gmail.com>
2023-04-13 23:43:22 +0900

    front: update configs

---
+++ frontend/src/components/pages/Sample.page.tsx
@@ -0,0 +1,10 @@
+import { Typography, Box } from '@mui/material'
+
+export const SamplePage = () => {
+  return (
+    <Box component="main">
+      <Typography variant="h3">Sample</Typography>
+      <Typography variant="body1">Sample page of this web app.</Typography>
+    </Box>
+  )
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     watch: {
       usePolling: true,
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "#": resolve(__dirname, "src"),
+    },
+  },
 })
