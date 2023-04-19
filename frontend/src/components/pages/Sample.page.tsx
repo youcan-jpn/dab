12e639d780a7e85bcec711815d52e42a73b66733
yuta ono <youcana40627@gmail.com>
2023-04-20 00:40:57 +0900

    front: add sample of react-query

--- frontend/src/components/pages/Sample.page.tsx
+++ frontend/src/components/pages/Sample.page.tsx
@@ -1,10 +1,18 @@
 import { Typography, Box } from "@mui/material";
 
+import { useGetShops } from "#/gen/queries/shops/shops";
+import type { Shop } from "#/gen/models";
+
 export const SamplePage = () => {
+  const {data: res} = useGetShops();
+  console.log(res?.data);
   return (
     <Box component="main">
       <Typography variant="h3">Sample</Typography>
       <Typography variant="body1">Sample page of this web app.</Typography>
+      {res?.data.shops?.map((shop: Shop) => (
+        <p key={shop.shop_id}>{shop.shop_id}: {shop.shop_name}</p>
+      ))}
     </Box>
   );
 };
