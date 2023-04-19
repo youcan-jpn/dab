440d375de6d12312fd28f3cc037e01ac00724efa
yuta ono <youcana40627@gmail.com>
2023-04-20 00:40:38 +0900

    front: add QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
+import axios from 'axios';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
 import { SamplePage } from "#/components/pages/Sample.page";
 import "./App.css";
 
+axios.defaults.baseURL = "http://localhost:1323/api";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Hello</h1>
       <h2>Here is DAB page</h2>
       <SamplePage />
-    </>
+    </QueryClientProvider>
   );
 }
 
